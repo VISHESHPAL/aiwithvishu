8995e9f375f3e303d981d0f29b0f7f3b3133ee27
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2026-07-21 21:31:07 +0530

    added the best style

--- frontend/src/components/PostDetail.jsx
+++ frontend/src/components/PostDetail.jsx
@@ -290,7 +290,7 @@ export default function PostDetail({
               src={post.image}
               alt={post.title}
               loading="lazy"
-              className="absolute inset-0 m-auto h-full object-cover"
+              className="absolute inset-0 m-auto h-full "
               onError={(e) => {
                 e.target.src = 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?auto=format&fit=crop&w=800&q=80';
               }}
